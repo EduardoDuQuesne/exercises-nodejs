c1d2132fb803985194faca2da030dae358a40bed
Eduardo DuQuesne <eduquesne@gmail.com>
2018-03-06 14:50:52 -0600

    Delete math.js

--- diceroll/lib/math.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports.randomInt = (min, max) => {
-    let highNum = max * min;
-    let total = Math.floor(Math.random() * (highNum - min + 1) + min);
-    return total;
-};
